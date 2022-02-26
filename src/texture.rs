e1eadf9c9ba9339bc2517e14ee8ad6865b03ea3a
Christian Döring <doering.christian.2001@gmx.de>
2022-02-26 11:59:06 +0100

    Added TextureBuilder.

--- src/texture.rs
+++ src/texture.rs
@@ -68,6 +68,148 @@ impl<'ts> TextureSlice<'ts>{
     }
 }
 
+pub struct TextureBuilder<'tb>{
+    pub data: Option<Vec<u8>>,
+    pub size: wgpu::Extent3d,
+    pub sampler_descriptor: wgpu::SamplerDescriptor<'tb>,
+    pub usage: wgpu::TextureUsages,
+    pub format: wgpu::TextureFormat,
+    pub dimension: wgpu::TextureDimension,
+    pub label: wgpu::Label<'tb>,
+}
+
+impl<'tb> TextureBuilder<'tb>{
+
+    pub fn new() -> Self{
+        let sampler_descriptor = wgpu::SamplerDescriptor{
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Linear,
+                min_filter: wgpu::FilterMode::Linear,
+                mipmap_filter: wgpu::FilterMode::Linear,
+                ..Default::default()
+        };
+        
+        let usage = wgpu::TextureUsages::TEXTURE_BINDING
+                | wgpu::TextureUsages::COPY_DST
+                | wgpu::TextureUsages::COPY_SRC
+                | wgpu::TextureUsages::RENDER_ATTACHMENT;
+
+        let format = wgpu::TextureFormat::Rgba8Unorm;
+
+        let dimension = wgpu::TextureDimension::D2;
+
+        Self{
+            data: None,
+            size: wgpu::Extent3d::default(),
+            sampler_descriptor,
+            usage,
+            format,
+            dimension,
+            label: None,
+        }
+    }
+
+    pub fn from_raw(mut self, data: Vec<u8>, size: wgpu::Extent3d) -> Self{
+        self.data = Some(data);
+        self.size = size;
+        self
+    }
+
+    pub fn from_image(mut self, img: &image::DynamicImage) -> Self{
+        let img_data: Vec<u8> = match self.format{
+            wgpu::TextureFormat::Rgba8Unorm     => img.flipv().to_rgba8().into_raw(),
+            wgpu::TextureFormat::Rgba8UnormSrgb => img.flipv().to_rgba8().into_raw(),
+            wgpu::TextureFormat::Bgra8Unorm     => img.flipv().to_bgra8().into_raw(),
+            wgpu::TextureFormat::Bgra8UnormSrgb => img.flipv().to_bgra8().into_raw(),
+            _ => {
+                panic!("TextureFormat not supported")
+            }
+        };
+        let dims = img.dimensions();
+
+        let extent = wgpu::Extent3d{
+            width: dims.0,
+            height: dims.1,
+            depth_or_array_layers: 1,
+        };
+        self.data = Some(img_data);
+        self.size = extent;
+        self
+    }
+
+    pub fn from_bytes(self, bytes: &[u8]) -> Self{
+        let img = image::load_from_memory(bytes).unwrap();
+        Self::from_image(self, &img)
+    }
+
+    pub fn load_from_path(self, path: &str) -> Self{
+        let mut f = File::open(path).unwrap();
+        let metadata = fs::metadata(path).unwrap();
+        let mut buffer = vec![0; metadata.len() as usize];
+        f.read(&mut buffer).unwrap();
+        Self::from_bytes(self, &buffer)
+    }
+
+    pub fn label(mut self, label: wgpu::Label<'tb>) -> Self{
+        self.label = label;
+        self
+    }
+
+    pub fn build(&mut self, device: &wgpu::Device, queue: &wgpu::Queue) -> Texture{
+        let texture = device.create_texture(
+            &wgpu::TextureDescriptor{
+                label: self.label,
+                size: self.size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: self.format,
+                usage: wgpu::TextureUsages::TEXTURE_BINDING
+                    | wgpu::TextureUsages::COPY_DST
+                    | wgpu::TextureUsages::COPY_SRC
+                    | wgpu::TextureUsages::RENDER_ATTACHMENT
+            }
+        );
+        let texture_view_desc = wgpu::TextureViewDescriptor{
+            format: Some(self.format),
+            ..Default::default()
+        };
+        let view = texture.create_view(&texture_view_desc);
+        let sampler = device.create_sampler(
+            &self.sampler_descriptor
+        );
+
+        if let Some(data) = &self.data{
+            queue.write_texture(
+                wgpu::ImageCopyTexture{
+                    aspect: wgpu::TextureAspect::All,
+                    texture: &texture,
+                    mip_level: 0,
+                    origin: wgpu::Origin3d::ZERO,
+                },
+                data,
+                wgpu::ImageDataLayout{
+                    offset: 0,
+                    bytes_per_row: std::num::NonZeroU32::new(4 * self.size.width),
+                    rows_per_image: std::num::NonZeroU32::new(self.size.height),
+                },
+                self.size,
+            );
+        }
+
+        Texture{
+            texture,
+            view,
+            sampler,
+            format: self.format,
+            size: self.size,
+        }
+    }
+
+}
+
 impl Texture{
     pub fn load_from_path(
         device: &wgpu::Device, 
