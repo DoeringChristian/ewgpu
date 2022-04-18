2fb4a62a412db3c8e76567e96f3012f374200350
Christian Döring <doering.christian.2001@gmx.de>
2022-04-18 13:50:06 +0200

    Added egui context not yet working.

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -3,8 +3,12 @@ pub mod gpu_context;
 pub mod winit_context;
 #[cfg(feature = "imgui")]
 pub mod imgui_context;
+#[cfg(feature = "egui")]
+pub mod egui_context;
 
 pub use gpu_context::*;
 pub use winit_context::*;
 #[cfg(feature = "imgui")]
 pub use imgui_context::*;
+#[cfg(feature = "egui")]
+pub use egui_context::*;
